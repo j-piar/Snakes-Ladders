ded259773747e1f2394c90f2aba6ed1be3765202
j-piar <juraj2.piar@live.uwe.ac.uk>
2015-04-02 11:06:40 +0100

    .

--- src/SnakesAndLadders/GameMain.java
+++ src/SnakesAndLadders/GameMain.java
@@ -1,13 +1,54 @@
 package SnakesAndLadders;
 
+import java.awt.Point;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
 /**
  *
  * @author
  */
-public class GameMain {
-
+public class GameMain extends javax.swing.JFrame
+{
+    private JLabel jLabel1;
+    private int[][] boardGrid;
+    
     public static void main(String[] args) {
-        Board board = new Board(5, 2);
+        Board board = new Board(5, 10);
+        System.out.println("role: " + board.getTileRole(new Point(0,0)));
+        
+        SwingUtilities.invokeLater(() ->
+        {
+            GameMain inst = new GameMain();
+            inst.setLocationRelativeTo(null);
+            inst.setVisible(true);
+        });
     }
-
+    public GameMain()
+    {
+        super();
+        initGUI();
+    }
+    
+    private void initGUI()
+    {
+        try
+        {
+            setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            getContentPane().setLayout(null);
+            {
+                jLabel1 = new JLabel();
+                getContentPane().add(jLabel1);
+                jLabel1.setText("jLabel1");
+                jLabel1.setIcon(null);
+                jLabel1.setBounds(199, 0, 742, 484);
+            }
+            pack();
+            this.setSize(963, 523);
+        }catch (Exception e)
+        {
+        }
+        
+    }
 }
